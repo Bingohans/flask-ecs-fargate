d03d7bb3e47d36e7fc5166992d299b420f725148
Bingohans <74136219+Bingohans@users.noreply.github.com>
2025-11-18 17:00:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home():
 
 @app.route('/health')
 def health():
-    return jsonify(status="healthy"), 200   # <--- DET HER ER NØGLEN
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)

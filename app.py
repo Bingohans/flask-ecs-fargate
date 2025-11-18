c0eb82a0cada9b2aa558c76b91725afe3a95e25c
Bingohans <74136219+Bingohans@users.noreply.github.com>
2025-11-18 17:33:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home():
 
 @app.route('/health')
 def health():
-    return {"status": "healthy": "200"}, 200
+    return {"status": "healthy"}, 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
